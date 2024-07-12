134ac050a2c6433d742448456b7c84790f515137
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-07-12 11:46:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // ganti info bot dibawah ini
-global.botName = "Xtree 2.9"
-global.ownerName = "BLANK"
-global.botNumber = "6289510342073"
-global.devNumber = ["6282230049011"]
+global.botName = "Strava"
+global.ownerName = "✘ᴸᴼᴿᴰᴛᴀɪʀᴀ✘"
+global.botNumber = "218912417014"
+global.devNumber = ["2347080968564"]
 
 global.Auto_Typing = false // auto typing
-global.Auto_Recording = false // auto recording
+global.Auto_Recording = true // auto recording
 global.Auto_ReadPesan = false // auto read messages
 
-global.domain = "https://example.id" // Ganti Domain Lu
+global.domain = "https://whatsapp.com/channel/0029VaY0Zq32P59piTo5rg0K" // Ganti Domain Lu
 global.key_plta = "-" // Isi Apikey Plta Lu
 global.key_pltc = "-" // Isi Apikey Pltc Lu
 global.nama_host = "LECCYOFC" // Ganti Nama Store Atau nama Host Lu
